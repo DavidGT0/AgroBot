b80472714ba62f10b04cadaf14b14da7ced12902
yehuda-d <yehuda130@gmail.com>
2026-01-23 11:06:00 +0200

    update the index js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const express = require('express');;
+const express = require('express');
+require('dotenv').config();
+const db = require('./config/db_config');
+const app = express();
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+
+
+app.listen(port, () => {console.log(`http://${host}:${port}`);});
